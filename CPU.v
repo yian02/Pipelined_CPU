084182c94f8c311280de7055da2ec3826693c3ff
Yian <brian28053849@gmail.com>
2021-05-24 04:30:18 +0800

    add DataMemory

---
+++ CPU.v
@@ -0,0 +1,94 @@
+module CPU
+(
+    clk_i, 
+    rst_i,
+    start_i
+);
+
+// Ports
+input               clk_i;
+input               rst_i;
+input               start_i;
+
+Control Control(
+    .Op_i       (Instruction_Memory.instr_o[6:0]),
+    .ALUOp_o    (),
+    .ALUSrc_o   (),
+    .RegWrite_o ()
+);
+
+Adder Add_PC(
+    .data1_in   (PC.pc_o),
+    .data2_in   (32'd4),
+    .data_o     ()
+);
+
+PC PC(
+    .clk_i      (clk_i),
+    .rst_i      (rst_i),
+    .start_i    (start_i),
+    .pc_i       (Add_PC.data_o),
+    .pc_o       (),
+    .PCWrite_i (1'b1)// ###########must remove#############
+
+);
+
+Instruction_Memory Instruction_Memory(
+    .addr_i     (PC.pc_o), 
+    .instr_o    ()
+);
+
+Registers Registers(
+    .clk_i      (clk_i),
+    .RS1addr_i  (Instruction_Memory.instr_o [19:15]),
+    .RS2addr_i  (Instruction_Memory.instr_o [24:20]),
+    .RDaddr_i   (Instruction_Memory.instr_o [11:7]), 
+    .RDdata_i   (MUX_MemtoReg.data_o),
+    .RegWrite_i (Control.RegWrite_o), 
+    .RS1data_o   (), 
+    .RS2data_o   () 
+);
+
+MUX32 MUX_ALUSrc(
+    .data1_i    (Registers.RS2data_o),
+    .data2_i    (Sign_Extend.data_o),
+    .select_i   (Control.ALUSrc_o),
+    .data_o     ()
+);
+
+MUX32 MUX_MemtoReg(
+    .data1_i    (ALU.data_o),
+    .data2_i    (Data_Memory.data_o),
+    .select_i   (Control.MemtoReg_o),
+    .data_o      ()
+);
+
+Sign_Extend Sign_Extend(
+    .instruction_i     (Instruction_Memory.instr_o),
+    .data_o     ()
+);
+
+ALU ALU(
+    .data1_i    (Registers.RS1data_o),
+    .data2_i    (MUX_ALUSrc.data_o),
+    .ALUCtrl_i  (ALU_Control.ALUCtrl_o),
+    .data_o     (),
+    .Zero_o     ()
+);
+
+ALU_Control ALU_Control(
+    .funct_i    ({Instruction_Memory.instr_o[31:25],Instruction_Memory.instr_o[14:12]}),
+    .ALUOp_i    (Control.ALUOp_o),
+    .ALUCtrl_o  ()
+);
+
+Data_Memory Data_Memory(
+    .clk_i(clk_i), 
+    .addr_i (ALU.data_o), 
+    .MemRead_i (Control.MemRead_o),
+    .MemWrite_i (Control.MemWrite_o),
+    .data_i (Registers.RS2data_o),
+    .data_o ()
+);
+
+endmodule
---
+++ Control.v
@@ -0,0 +1,81 @@
+`define R_Type 7'b0110011
+`define I_Type 7'b0010011
+`define LW     7'b0000011
+`define SW     7'b0100011
+`define BEQ    7'b1100011
+
+module Control(
+    Op_i,
+    ALUOp_o,
+    ALUSrc_o,
+    RegWrite_o,
+    // to supprot data memory
+    MemtoReg_o,
+    MemRead_o,
+    MemWrite_o
+);
+
+input [6:0] Op_i;
+output reg [1:0] ALUOp_o;
+output reg ALUSrc_o;
+output reg RegWrite_o;
+output reg MemtoReg_o;
+output reg MemRead_o;
+output reg MemWrite_o;
+
+
+
+always@(Op_i)
+begin
+case (Op_i)
+    `R_Type: begin
+            ALUSrc_o <= 1'b0;
+            RegWrite_o <= 1'b1;
+            ALUOp_o <= 2'b00;
+            MemtoReg_o <= 1'b0;
+            MemRead_o <= 1'b0;
+            MemWrite_o <= 1'b0;
+        end    
+    `I_Type: begin
+            ALUSrc_o <= 1'b1;
+            RegWrite_o <= 1'b1;
+            ALUOp_o <=2'b10;
+            MemtoReg_o <= 1'b0;
+            MemRead_o <= 1'b0;
+            MemWrite_o <= 1'b0;
+        end
+    `LW: begin//not done
+            ALUSrc_o <= 1'b1;
+            RegWrite_o <= 1'b1;
+            ALUOp_o <=2'b10;
+            MemtoReg_o <= 1'b1;
+            MemRead_o <= 1'b1;
+            MemWrite_o <= 1'b0;
+        end    
+    `SW: begin//not done
+        //$fdisplay(1, "SW\n");
+            ALUSrc_o <= 1'b1;
+            RegWrite_o <= 1'b0;
+            ALUOp_o <=2'b10;
+            MemtoReg_o <= 1'b0;
+            MemRead_o <= 1'b0;
+            MemWrite_o <= 1'b1;
+        end    
+    `BEQ: begin//not done
+            ALUSrc_o <= 1'b1;
+            RegWrite_o <= 1'b0;
+            ALUOp_o <=2'b10;
+            MemtoReg_o <= 1'b0;
+            MemRead_o <= 1'b0;
+            MemWrite_o <= 1'b0;
+        end    
+    default:begin
+            RegWrite_o <= 1'b0;
+            MemtoReg_o <= 1'b0;
+            MemRead_o <= 1'b0;
+            MemWrite_o <= 1'b0;
+        end    
+endcase
+end
+
+endmodule
